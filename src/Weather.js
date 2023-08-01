4a48634c1078e780bce60a290233ebc269fd76d3
aprilleg <aprilleguevarra1@gmail.com>
2023-08-02 01:01:27 +0800

    Added footer
    Added github link repo

--- src/Weather.js
+++ src/Weather.js
@@ -3,7 +3,10 @@ import './Weather.css';
 export default function Weather() {
   return (
     <div className="App">
-      
+      <footer>
+        This project was coded by Aprille Guevarra and is{" "}
+        <a href="#">open-source on Github</a>
+      </footer>
     </div>
   );
 }
