b0e8a3daf087582017995e978c5f729e9cd56b1c
aprilleg <aprilleguevarra1@gmail.com>
2023-08-02 01:11:42 +0800

    Added bootstrap

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import "bootstrap/dist/css/bootstrap.css";
 import Weather from './Weather';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
